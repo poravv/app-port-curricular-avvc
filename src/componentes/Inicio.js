72aea54205d8c1de500bb5735cb3417f8cb26385
Andres Vera <andresvera@MacBook-Pro-de-Andres.local>
2022-10-09 00:18:06 -0300

    validado 2

--- src/componentes/Inicio.js
+++ src/componentes/Inicio.js
@@ -7,17 +7,17 @@ export const Inicio = () => {
         <section>
             <div className="personal">
                 <Container>
-                    <h1>Bienvenido a mi sitio</h1>
-                    <p>Aqui encontraras todo lo relacionado con mi experiencia profesional en el rubro de 
-                        la informatica y otros cursos y conocimientos extra que eh ido adquiriendo al paso 
-                        de los años. Sin mas que decir, espero la información te sea de utilidad para lo 
-                        que estas buscando</p>
-                        <p style={{fontweight: `bold`}}>
-                        Andrés Valentin Vera Chavez
-                        </p>
+                    <img alt="img1" src={Imagen} />
                 </Container>
                 <Container>
-                    <img alt="img1" src={Imagen} />
+                    <h1>Bienvenido a mi sitio</h1>
+                    <p>Aqui encontraras todo lo relacionado con mi experiencia profesional en el rubro de
+                        la informatica y otros cursos y conocimientos extra que eh ido adquiriendo al paso
+                        de los años. Sin mas que decir, espero la información te sea de utilidad para lo
+                        que estas buscando</p>
+                    <p style={{ fontweight: `bold` }}>
+                        Andrés Valentin Vera Chavez
+                    </p>
                 </Container>
             </div>
         </section>
