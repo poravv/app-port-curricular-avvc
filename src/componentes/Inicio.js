fa54fb6af7e8397456e9767e334868bef8d34da0
Andres Vera <andresvera@MacBook-Pro-de-Andres.local>
2022-10-09 15:05:43 -0300

    validado 2

--- src/componentes/Inicio.js
+++ src/componentes/Inicio.js
@@ -6,7 +6,7 @@ export const Inicio = () => {
         <>
           <div className="personal" style={{display: `flex` , alignItems:`center`, justifyContent:`center`,textAlign:`center`  }}>
                 <div className='container'>
-                    <img alt="img1" src={Imagen} />
+                    <img className='img' alt="img1" src={Imagen} />
                 </div>
                 <div className='container'>
                     <h1>Bienvenido a mi sitio</h1>
