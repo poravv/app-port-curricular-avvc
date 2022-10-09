3cc1d040cb96fb832be7b3740854af9ff39c1cdd
Andres Vera <andresvera@MacBook-Pro-de-Andres.local>
2022-10-09 00:45:30 -0300

    validado 2

--- src/layouts/Footer.js
+++ src/layouts/Footer.js
@@ -5,7 +5,7 @@ import Ig from '../assets/ig.webp';
 function Footer() {
     return (
         <>
-            <footer className="page-footer font-small blue pt-4">
+            <footer className="page-footer font-small blue pt-4 bg-secondary">
                 <div className="footer-copyright text-center">
                     
                     <p href="/"> © 2022 Copyright: Andrés Vera</p>
