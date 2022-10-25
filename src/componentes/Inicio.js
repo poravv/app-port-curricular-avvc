fef7ca8d54a9e10ebae01af43527a6f044a9344f
Andres Vera <andresvera@MacBook-Pro-de-Andres.local>
2022-10-25 17:55:41 -0300

    Cambio de icono y nombre

--- src/componentes/Inicio.js
+++ src/componentes/Inicio.js
@@ -9,10 +9,11 @@ export const Inicio = () => {
                     <img className='img' alt="img1" src={Imagen} />
                 </div>
                 <div className='container'>
-                    <h1>Bienvenido a mi sitio</h1>
-                    <p>Aquí encontraras todo lo relacionado con mi experiencia profesional en el rubro de la informática y 
-                        otros cursos y conocimientos extra que he ido adquiriendo al paso de los años. Sin más que decir, 
-                        espero la información te sea de utilidad para lo que estas buscando.</p>
+                    <h1>Hola y bienvenido a mi sitio</h1>
+                    <p>Aquí encontraras todo lo referente a mi experiencia profesional en el rubro de la informática y 
+                        otros cursos y conocimientos extra que he ido adquiriendo al paso de los años. Sin más que añadir, 
+                        espero que la información te sea de utilidad para lo que estas buscando.</p>
+                    <p>Saludos cordiales</p>
                     <p style={{ fontweight: `bold` }}>
                         <b>Andrés Valentin Vera Chavez</b>
                     </p>
